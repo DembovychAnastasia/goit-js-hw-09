4b05b650b15d9c4be82367a0bb2f77e0a59a40ef
DembovychAnastasia <113368687+DembovychAnastasia@users.noreply.github.com>
2023-03-07 09:40:00 +0200

    add work js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+let intervalId;
 
+//Функція для зміни кольору - рандомна
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+//Функція зміни кольору
+function backgroundColorchange() {
+    document.body.style.background = getRandomHexColor();
+  }
+  
+  function startColorChanging() {
+    // Встановлення інтервалу зміни кольору
+    intervalId = setInterval(backgroundColorchange, 1000);
+    startButton.disabled = true;
+    stopButton.disabled = false;
+  }
+  
+  function stopColorChanging() {
+    clearInterval(intervalId);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+  }
+
+startButton.addEventListener('click', startColorChanging);
+stopButton.addEventListener('click', stopColorChanging);

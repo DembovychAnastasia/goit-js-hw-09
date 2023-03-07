6f9845f33a220b22a5ed316f55563a8ee498aaef
DembovychAnastasia <113368687+DembovychAnastasia@users.noreply.github.com>
2023-03-07 07:40:27 +0000

    Deploying to gh-pages from @ DembovychAnastasia/goit-js-hw-09@4b05b650b15d9c4be82367a0bb2f77e0a59a40ef 🚀

---
+++ 01-color-switcher.ac157359.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n;function d(){document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(function(){n=setInterval(d,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.ac157359.js.map
